313b15eb6621cc0f26174d7223d5ae0b88393419
= <buidiepanh0905@gmail.com>
2024-06-04 16:19:41 +0700

    Update

--- LittleAngelMilk/src/App.jsx
+++ LittleAngelMilk/src/App.jsx
@@ -1,6 +1,6 @@
-import LoginForm from "./page/LoginForm/LoginForm";
-import Register from "./page/RegisterForm/RegisterForm";
-import Header from "./header/Header";
+// import LoginForm from "./page/LoginForm/LoginForm";
+// import Register from "./page/RegisterForm/RegisterForm";
+// import Header from "./header/Header";
 import Carousel from "./component/carousel/carousel";
 import Footer from "./component/footer/footer";
 import Content from "./component/content/content";
