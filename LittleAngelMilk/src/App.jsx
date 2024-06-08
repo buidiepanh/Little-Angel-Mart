4e25642e23e49832011bfad88b18bb4908c872a1
HOANGHUNG\ROG <warriror21@gmail.com>
2024-06-08 10:50:56 +0700

    update

--- LittleAngelMilk/src/App.jsx
+++ LittleAngelMilk/src/App.jsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// import LoginForm from "./page/LoginForm/LoginForm";
-// import Register from "./page/RegisterForm/RegisterForm";
-// import Header from "./header/Header";
-import Carousel from "./component/carousel/carousel";
-import Footer from "./component/footer/footer";
-import Content from "./component/content/content";
-||||||| 151d472
-import LoginForm from "./page/LoginForm/LoginForm";
-import Register from "./page/RegisterForm/RegisterForm";
-import Header from "./header/Header";
-import Carousel from "./component/carousel/carousel";
-import Footer from "./component/footer/footer";
-=======
 import Header from "./component/header/Header";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
@@ -19,22 +5,9 @@ import LoginForm from "./page/LoginForm/LoginForm";
 import RegisterForm from "./page/RegisterForm/RegisterForm";
 import Home from "./page/Home/Home";
 import ForgotPage from "./page/ForgotPage/ForgotPage";
->>>>>>> main
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <Carousel />
-      <Content />
-      <Footer />
-    </div>
-||||||| 151d472
-    <div>
-      <Carousel />
-      <Footer />
-    </div>
-=======
     <ForgotPage />
     // <Router>
     //     <div>
@@ -45,7 +18,6 @@ const App = () => {
     //         </Routes>
     //     </div>
     // </Router>
->>>>>>> main
   );
 };
 
