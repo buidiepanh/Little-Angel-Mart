4a278b28e7f285090074ebdb4bccb7316e31f8ec
nguyendang03 <npmd2004@gmail.com>
2024-06-08 23:49:54 +0700

    delete non-functional home page

--- LittleAngelMilk/src/App.jsx
+++ LittleAngelMilk/src/App.jsx
@@ -4,21 +4,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import LoginForm from './page/LoginForm/LoginForm';
 import RegisterForm from './page/RegisterForm/RegisterForm';
-import Home from "./page/Home/Home";
 import ForgotPage from "./page/ForgotPage/ForgotPage";
-
+import ForgotPagePassword from "./page/ForgotPagePassword/ForgotPagePassword";
 const App = () => {
     return (
-      <ForgotPage/>
-        // <Router>
-        //     <div>
-        //         <Routes>
-        //           <Route path="/" element={<Home/>} />
-        //             <Route path="/login" element={<LoginForm />} />
-        //             <Route path="/register" element={<RegisterForm />} />
-        //         </Routes>
-        //     </div>
-        // </Router>
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/ForgotPassEmail" element={<ForgotPage/>}/>
+                    <Route path="/ForgotPass" element={<ForgotPagePassword/>}/>
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/register" element={<RegisterForm />} />
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
--- LittleAngelMilk/src/page/ForgotPagePassword/ForgotPagePassword.jsx
+++ LittleAngelMilk/src/page/ForgotPagePassword/ForgotPagePassword.jsx
@@ -1,36 +1,44 @@
 import React from 'react'
-import "./ForgotPage.css";
+import "./ForgotPagePassword.css";
 import { Link } from 'react-router-dom';
 import LOGO from "../../assets/Logo.jpg";
 export default function ForgotPage() {
   return (
     <div className="container">
-      <div className='ForgotPassEmailForm'>
+      <div className='ForgotPassForm'>
       <div className="image-container">
         <img src={LOGO} alt="Logo" />
       </div>
       <div className="form-container">
         <div className='FormTitle'>
         <p className='FormHead'>Quên mật khẩu</p>
-        <p>Hãy nhập email của bạn</p>
+        <p>Hãy nhập mật khẩu mới</p>
         </div>
         <form className="loginForm" onSubmit={(e) => e.preventDefault()}>
         
-        <div className="emailInput">
-        <p><b>Email</b></p>
+        <div className="NewPasswordInput">
+        <p><b>Mật khẩu mới</b></p>
             <input
               type="text"
               id="userEmail"
-              placeholder="example@gmail.com"
+              placeholder="Nhập mật khẩu mới"
               required
             />
-            <Link to="/ForgotPass">
-            <button type="submit" className="ForgotPassEmailBtn">
-            Tiếp tục
+             <p><b>Xác minh mật khẩu mới</b></p>
+            <input
+              type="text"
+              id="userEmail"
+              placeholder="Nhập lại mật khẩu mới"
+              required
+            />
+        </div> 
+        <div className="btnClass">
+       
+            <button type="submit" className="ResetPassBtn">
+            Đặt lại mật khẩu
             </button>
-            </Link>
-          </div>       
-          </form>
+        </div>      
+        </form>
       </div>
       </div>     
     </div>
