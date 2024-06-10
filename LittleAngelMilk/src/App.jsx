13de4e077f8324c93540d7637e5042f5c879d04e
nguyendang03 <npmd2004@gmail.com>
2024-06-10 13:14:47 +0700

    delete backend folder

--- LittleAngelMilk/src/App.jsx
+++ LittleAngelMilk/src/App.jsx
@@ -1,23 +1,27 @@
 
 
-import './page/ProductionDetail/ProductionDetail.css'; 
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginForm from './page/LoginForm/LoginForm';
 import ProductionDetail from './page/ProductionDetail/ProductionDetail';
 import RegisterForm from './page/RegisterForm/RegisterForm';
 import ForgotPage from "./page/ForgotPage/ForgotPage";
 import ForgotPagePassword from "./page/ForgotPagePassword/ForgotPagePassword";
+import CartPage from './page/CartPage/CartPage';
 const App = () => {
     return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/ForgotPassEmail" element={<ForgotPage/>}/>
-                    <Route path="/ForgotPass" element={<ForgotPagePassword/>}/>
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/register" element={<RegisterForm />} />
-                </Routes>
-            </div>
-        </Router>
+      // <ProductionDetail/>
+      <CartPage/>
+        // <Router>
+        //     <div>
+        //         <Routes>
+        //             <Route path="/ForgotPassEmail" element={<ForgotPage/>}/>
+        //             <Route path="/ForgotPass" element={<ForgotPagePassword/>}/>
+        //             <Route path="/login" element={<LoginForm />} />
+        //             <Route path="/register" element={<RegisterForm />} />
+        //         </Routes>
+        //     </div>
+        // </Router>
     );
 };
 
--- backend/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import Product from "./Product.schema";
-import Category from "./Category.schema";
-import User from "./User.schema";
-
-export const lists = {
-  Product,
-  User,
-  Category,
-};
