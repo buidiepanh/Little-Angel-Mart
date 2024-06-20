b8c7887bf93058e00f540a57edf9d5c8c204c35d
LAPTOP-ICSQ49FB\ADMIN <tuanbin2982003@gmail.com>
2024-06-20 18:46:24 +0700

    Call Api and render into UI for article

--- LittleAngelMilk/src/page/ProductionDetail/ProductionDetail.jsx
+++ LittleAngelMilk/src/page/ProductionDetail/ProductionDetail.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import './ProductionDetail.css';
-import productImage from '../../image/binhsua.jpg';
-import ProductCounter from '../../component/ProductionDetail/ProductCounter'; 
+import React, { Component } from "react";
+import "./ProductionDetail.css";
+import productImage from "../../image/binhsua.jpg";
+import ProductCounter from "../../component/ProductionDetail/ProductCounter";
 
 class ProductionDetail extends Component {
   render() {
@@ -14,21 +14,27 @@ class ProductionDetail extends Component {
           <div className="product-info">
             <h1>Hộp chia sữa 3 tầng tiện lợi</h1>
             <div className="product-rating">
-                <span>Rating:</span>
-                ★★★★☆
+              <span>Rating:</span>
+              ★★★★☆
             </div>
             <div className="product-price">109.000đ</div>
             <ProductCounter />
             <div className="product-actions">
               <button className="button-large btn-buy">Mua ngay</button>
-              <button className="button-large btn-cart">Thêm vào giỏ hàng</button>
+              <button className="button-large btn-cart">
+                Thêm vào giỏ hàng
+              </button>
             </div>
           </div>
         </div>
         <div className="product-lower">
           <div className="product-description">
-          <h2>Mô tả sản phẩm</h2> 
-          Hộp chia sữa 3 tầng tiện lợi Dottodot là món đồ tiện lợi mà các bà mẹ có con nhỏ không nên bỏ qua. Sản phẩm được làm từ nhựa cao cấp chịu nhiệt tốt, giúp các mẹ bỉm bảo quản vẹn nguyên dòng sữa nguyên chất cho con. Thiết kế hộp 3 tầng có thể tháo lắp dễ dàng, giúp ba mẹ tiện lợi mang theo khi đưa bé đi chơi xa.
+            <h2>Mô tả sản phẩm</h2>
+            Hộp chia sữa 3 tầng tiện lợi Dottodot là món đồ tiện lợi mà các bà
+            mẹ có con nhỏ không nên bỏ qua. Sản phẩm được làm từ nhựa cao cấp
+            chịu nhiệt tốt, giúp các mẹ bỉm bảo quản vẹn nguyên dòng sữa nguyên
+            chất cho con. Thiết kế hộp 3 tầng có thể tháo lắp dễ dàng, giúp ba
+            mẹ tiện lợi mang theo khi đưa bé đi chơi xa.
           </div>
           <div className="product-recommendations">
             <h2>Các sản phẩm tương tự</h2>
